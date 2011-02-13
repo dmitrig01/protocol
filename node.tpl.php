fcb3be3d6f70be73c1fe1beb7669d48303b7248c
Dmitri Gaskin <dmitrig01@gmail.com>
2011-02-12 16:55:14 -0800

    Remove submitted text

--- node.tpl.php
+++ node.tpl.php
@@ -56,16 +56,6 @@
   <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
 <?php endif; ?>
 
-  <div class="meta">
-  <?php if ($submitted): ?>
-    <span class="submitted"><?php print $submitted ?></span>
-  <?php endif; ?>
-
-  <?php if ($terms): ?>
-    <div class="terms terms-inline"><?php print $terms ?></div>
-  <?php endif;?>
-  </div>
-
   <div class="content">
     <?php print $content ?>
   </div>
